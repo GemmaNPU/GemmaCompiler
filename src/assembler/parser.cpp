b3e2ff775a3ddac659b704cd822c0ea69ab01406
GemmaNPU <gemmanpu@gmail.com>
2024-08-12 22:10:38 +0000

    Made more similar to the example on cppreference

--- src/assembler/parser.cpp
+++ src/assembler/parser.cpp
@@ -30,7 +30,7 @@
           while ( token.getKind() == Token::Kind::NUMBER && current_option_index < 3 ) {
             uint64_t parse_result;
             [[ maybe_unused ]]
-            std::from_chars_result chars_result = std::from_chars(token.getLexeme().data(), token.getLexeme().end(), parse_result);
+            std::from_chars_result chars_result = std::from_chars(token.getLexeme().data(), token.getLexeme().data() + token.getLexeme().size(), parse_result);
             // TODO: add checks for conversion errors
             options[current_option_index] = parse_result;
             current_option_index++;
